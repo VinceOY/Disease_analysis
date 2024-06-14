91c47fcd0be8a709a0af561fc3d61e714fcb2f1f
VinceOY <foxcloud0726@gmail.com>
2024-06-14 15:50:39 +0800

    modified README.md

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ Disease_analysis/
     - input_files: v_labresult_t.csv、v_labresult_s.csv.csv、v_exper_sign_w.csv
     - output_table: 三院資料整合與重新命名欄位(lab_result.csv) * 2test_items
 
-- create_table.R: (case by case)
+- create_table.R: done
   - 目的: 資料摘要表(包括全體、有檢驗項目、及條件篩選過後的摘要表)
     - input: outcome_clean.csv、lab_result.csv
     - output: 
@@ -36,7 +36,7 @@ Disease_analysis/
     - output_table3: 目標病患在各test_items對應到各outcome總數量、發病數量以及總追蹤時間
     - output_table 4~15: 最後分析人選的檢驗資料(含基本資料) (test_outcome.csv) * 12 
 
-- cox_model_result.R: (case by case)
+- cox_model_result.R: done
   - 目的: 2個檢驗項目, 3種變數組合, 6個併發症用cox model進行建模&分析 
     - input_table: test_outcome.csv
     - output_table: 36種模型結果整合 (model_result.csv)
--- cox_model_result_renew.R
+++ cox_model_result_renew.R
@@ -4,6 +4,29 @@ setwd(new_dir)
 library(data.table)
 library(stringr)
 
+#===============================================================================
+# define parameters: 
+# Test_item: test_item name, unit
+# exclude_columns: 清除age, ID, index_date
+# target_folder_path: 檔案存放路徑
+# outcome_diseases: DM related diseases
+parameters <- list(
+  target_folder_path = "C:/Users/USER/Downloads/disease_df/",
+  Test_item = list(HbA1c = list(ID = c("014701","F09006B"), unit = c("%")),
+                   ALBUMIN = list(ID = c("010301","11D101","F09038C"), 
+                                  unit = c("(?i) g/dl"))),
+  exclude_columns = c("exclude_AGE", "exclude_ID", "exclude_Indexdate"),
+  outcome_diseases = c("EyeComp", "CardioDisease", "CerebroDisease", 
+                       "PeripheralVascDisease", "Nephropathy", "DiabeticNeuro")
+)
+exclude_columns <- parameters$exclude_columns
+target_folder_path <- parameters$target_folder_path
+outcome <- parameters$outcome_diseases
+Test_item <- parameters$Test_item
+outcome_file <- paste0(outcome[1],"_clean.csv") # for table1 & table2 
+
+
+
 #=================================================================
 # cal mean by ppl 
 # input: clean_people_lab.csv 最終人選的檢驗資料(含基本資料) * 12 files (test+outcome)
